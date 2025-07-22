936c89de8d9e83078cf3b1f4bdc292e60308a9db
usersenseMS <swennenmichael@gmail.com>
2025-07-22 13:42:28 +0200

    Update HRV-tool.py

--- HRV-tool.py
+++ HRV-tool.py
@@ -87,6 +87,7 @@ if uploaded_file:
         height=350,
         margin=dict(l=10, r=10, t=40, b=20),
         hovermode='x unified'
+        yaxis=dict(range=[45, 85])
     )
 
     fig.update_xaxes(rangeslider_visible=True)
